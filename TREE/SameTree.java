8b0738738e4619e2f84f7fb8d250301b5b4a32df
sonu kumar <gautamrocky909621@gmail.com>
2026-06-20 15:56:56 +0530

    add same tree problem

---
+++ TREE/SameTree.java
@@ -0,0 +1,30 @@
+package TREE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while (!queue.isEmpty()) {
+            TreeNode first = queue.poll();
+            TreeNode second = queue.poll();
+            if (first == null && second == null) {
+                continue;
+            } else if ((first == null && second != null) || (first != null && second == null)) {
+                return false;
+            } else if (first == null || second == null || first.val != second.val) {
+                return false;
+            }
+            queue.add(first.left);
+            queue.add(second.left);
+            queue.add(first.right);
+            queue.add(second.right);
+            ;
+        }
+        return true;
+    }
+
+}
