9af7426db77cfa8efc4cd5e5af02198813522ade
cs-satyam <satyamwork4464@gmail.com>
2026-06-22 14:12:41 +0530

    Add Sonu method to Main class and call minu in main method

--- POD/Main.java
+++ POD/Main.java
@@ -2,9 +2,13 @@ package POD;
 
 public class Main {
     public static void main(String[] args) {
-
         
         maxIceCream maxIceCream = new maxIceCream();
         maxIceCream.max_IceCream(new int[]{1,3,2,4,1}, 7);
-    }
+
+        Sonu();
+     }
+     static void Sonu(){
+        System.out.println("Hello World");
+     }
 }
